9e6cf23946250fd516aa6b2005d2abde83d2d215
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2023-09-18 23:41:49 +0100

    no error message
    wont let me check if the integer is a string

--- Python challenge 3.py
+++ Python challenge 3.py
@@ -1,15 +1,17 @@
 #python challenge 3
 
 number = int(input("Enter your number here::: "))
+#creates variable
 
 if number < 0:
     print("Your number is negative")
-#if the number is lesser than 0
+#checks that the number is lesser than 0
     
 elif number == 0:
     print("Your number is zero")
-#if the number is 0
-    
+#checks that the number is equal to 0
+
 elif number > 0:
     print("Your number is positive")
-#if the number is grater than 0
+#checks that the number is greater than 0
+
