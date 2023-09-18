fa66bf165fa9bf078bcf283451fb820df0f8464e
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2023-09-18 22:46:10 +0100

    start

---
+++ for loop example 1.py
@@ -0,0 +1,17 @@
+#for loop example
+
+for count in range(10):
+    print(count)
+print()
+#goes from 0 to 9
+#to print'10' the range needs to be 11
+
+for x in range(1,10):
+    print(x)
+print()
+#goes from 1 to 9
+
+for y in range(3,36,3):
+    print(y)
+#starts at 3 and goes up in 3s till 33 
+
