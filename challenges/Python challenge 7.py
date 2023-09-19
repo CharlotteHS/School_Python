8ff1ff264dbaaa6ce8ccec391eba076f92733761
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2023-09-19 22:28:49 +0100

    challenge 7

--- challenges/Python challenge 7.py
+++ challenges/Python challenge 7.py
@@ -1,9 +1,12 @@
 #python challenge 7
 import random
+#enables random variables
 
 rps = ["rock", "paper", "scissors"]
+#list with all the options
 
 computer_move = random.choice(rps)
+#setting the variable to choose randomly from the list
 
 player_move = input("Enter your move here::: ")
 
@@ -18,4 +21,27 @@ if player_move == "rock":
 
     if computer_move == "scissors":
         print("You win")
-        
+
+if player_move == "paper":
+    print(computer_move)
+
+    if computer_move == "rock":
+        print("You win")
+
+    if computer_move == "paper":
+        print("Its a draw")
+
+    if computer_move == "scissors":
+        print("Computer wins")
+
+if player_move == "scissors":
+    print(computer_move)
+
+    if computer_move == "rock":
+        print("Computer wins")
+
+    if computer_move == "paper":
+        print("You win")
+
+    if computer_move == "scissors":
+        print("It's a draw")
