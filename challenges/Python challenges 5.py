893004957fff907e54d87742a11d99fe851386c0
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2023-09-19 19:30:40 +0100

    challenge 5
    elif is important

---
+++ challenges/Python challenges 5.py
@@ -0,0 +1,15 @@
+#python challenge 5
+
+grade = int(input("Enter your grade here::: "))
+
+if grade >= 75:
+    print("Grade: A")
+
+elif grade >= 60:
+    print("Grade: B")
+
+elif grade >= 35:
+    print("Grade: C")
+
+elif grade < 35:
+    print("Grade: D")
