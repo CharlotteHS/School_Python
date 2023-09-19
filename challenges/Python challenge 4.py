39d5042e9446dbe79646c89f8c486ef489bbc8bb
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2023-09-19 19:24:41 +0100

    challenge 4 done

---
+++ challenges/Python challenge 4.py
@@ -0,0 +1,12 @@
+#python challenge 4
+
+hours = int(input("How long do you spend watching TV each day on average? ::: "))
+
+if hours < 2:
+    print("That shouldn't rot your brain too much")
+
+elif hours <= 4:
+    print("Aren't you getting square eyes?")
+
+elif hours > 4:
+    print("Fresh air beats channel flicking")
