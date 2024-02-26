d6226fee00d438ded42b379760c695ba44598a20
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-02-26 21:19:05 +0000

    adding tickboxes

--- Tkinter/website textbox.py
+++ Tkinter/website textbox.py
@@ -32,10 +32,31 @@ age.grid(row=2, column=0)
 input_one = Text(width=20, height=1)
 input_one.grid(row=2, column=1, padx=10, pady=4)
 
+tos = IntVar()
+consent = IntVar()
+    #creating the variables
+tickbox1 = Checkbutton(screen, text="ToS", width = 30, variable = tos)
+tickbox1.grid(row=3, column=1, padx=1, pady=1)
+    #
+tickbox2 = Checkbutton(screen, text="Consent to save this information?", width = 30, variable = consent)
+tickbox2.grid(row=4, column=1, padx=1, pady=1)
+
 def cont():
     messagebox.showinfo("Loading...", "Signing in")
 
 sign_in = Button(screen, text="Sign In", command=cont)
-sign_in.grid(row=3, column=1, padx=0, pady=5)
+sign_in.grid(row=5, column=1, padx=1, pady=5)
+
+news = Label(screen, text="Would you like to recieve our newsletters?")
+news.grid(row=6, column=1, padx=1, pady=1)
+    #Creating a little heading thing
+
+news = StringVar()
+circle_tickbox = Radiobutton(screen, text="Yes", variable = news, value=1)
+circle_tickbox.grid(row=7, column=1, padx=1, pady=1)
+
+circle_tickbox2 = Radiobutton(screen, text="No", variable = news, value=2)
+circle_tickbox2.grid(row=8, column=1, padx=1, pady=1)
+
 
 screen.mainloop()
