19d221513ca03ed80f2e42d5466d224df882b8fd
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-02-26 20:29:41 +0000

    adding sign in button

--- Tkinter/website textbox.py
+++ Tkinter/website textbox.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import messagebox
 
 screen = Tk()
-screen.title("Website Sign in")
+screen.title("Website Sign up")
 screen.geometry("400x400")
 
 menubar = Menu(screen)
@@ -14,7 +14,7 @@ menubar.add_cascade(label="Help", menu=help_menu)
 
 app = Frame(screen)
 app.grid()
-#setting a grid the size of the screens frame
+    #setting a grid the size of the screens frame
 
 name = Label(screen, text="Name:")
 name.grid(row=0, column=0)
@@ -27,6 +27,7 @@ input_one = Text(width=20, height=1)
 input_one.grid(row=1, column=1, padx=10, pady=4)
 
 age = Label(screen, text="Age:")
+    #can try and get a toggle to pick the day, month, and year
 age.grid(row=2, column=0)
 input_one = Text(width=20, height=1)
 input_one.grid(row=2, column=1, padx=10, pady=4)
@@ -34,4 +35,9 @@ input_one.grid(row=2, column=1, padx=10, pady=4)
 def cont():
     messagebox.showinfo("Loading..., Signing in")
 
-cont = Button(screen)
+sign_in = Button(screen, text="Sign In", command=cont)
+sign_in.grid(row=5, column=1)
+
+
+
+screen.mainloop()
